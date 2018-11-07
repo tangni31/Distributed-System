e9c1536d82bfd401a889b83ceea2aefa97643f4d
Ni <tangni31@gmail.com>
2018-11-07 18:33:05 -0500

    Update control.py

--- project/control.py
+++ project/control.py
@@ -14,31 +14,29 @@ serverNames = ['localhost', 'localhost', 'localhost']
 serverPorts = [4000, 5000, 6000]
 
 LOCK = threading.Lock()
-INDEX = 0
+
 BUFSIZ = 1024
 
 
-def algorithm(ind, time):
-    global INDEX
-    if argv[1] == "EQ":
-        res = equal()
-    elif argv[1] == 'RR':
-        res = roundRobin(ind)
-    else:
-        res = sensibleRouting(ind, time, ALPHA)
-    LOCK.acquire()
-    INDEX = res
-    LOCK.release()
+def algorithm(ind, time, INDEX):
+    pass
+    #if argv[1] == "EQ":
+    #res = equal()
+    #elif argv[1] == 'RR':
+    #INDEX[0] = roundRobin(ind)
+    #else:
+    #res = sensibleRouting(ind, time, ALPHA)
 
 
-def task_gen(ind, RUN_TIME):
+def task_gen(RUN_TIME, INDEX):
     #data = random.randint(90000,120000)
     data = 100000
-    send(str(data), ind, RUN_TIME)
+    send(str(data), RUN_TIME, INDEX)
 
 
-def send(data, ind, RUN_TIME):
+def send(data,RUN_TIME, INDEX):
     global TOTAL_RUN_TIME
+    ind = INDEX[0]
     clientSocket = socket(AF_INET, SOCK_STREAM)
     serverName, serverPort = serverNames[ind],serverPorts[ind]
     addr = (serverName, serverPort)
@@ -48,19 +46,23 @@ def send(data, ind, RUN_TIME):
     returnData = clientSocket.recv(BUFSIZ)
     clientSocket.close()
     runTime = float(returnData)
-    algorithm(ind, runTime) #update host index
+    #algorithm(ind, runTime, INDEX), #update host index
     #print('Run time is: {:.2f} ms'.format(runTime))#float(returnData.decode('utf-8'))))
     RUN_TIME += runTime,
 
 
-def assign_task(RUN_TIME):
-    worker = threading.Thread(target=task_gen(INDEX, RUN_TIME), name='workerThread')
+
+def assign_task(RUN_TIME, INDEX):
+    worker = threading.Thread(target=task_gen(RUN_TIME, INDEX), name='workerThread')
     worker.start()
     worker.join()
 
 
 if __name__ == '__main__':
     manager = Manager()
+    #INDEX = manager.list()
+    #INDEX += 0,
+    INDEX = [0]
     RUN_TIME = manager.list()
     p = Pool(10)
     prv = time.time()
@@ -68,12 +70,14 @@ if __name__ == '__main__':
     while cnt < TOTAL_JOB:
         now = time.time()
         if now - prv > 1/JOB_RATE:
-            print (now-prv)
+            print(INDEX)
+            #print (now-prv)
             prv = now
-            p.apply_async(assign_task, args=(RUN_TIME,))
+            p.apply_async(assign_task, args=(RUN_TIME,INDEX,))
+            INDEX[0] += 1
+            INDEX[0] %= 3
             #p.apply_async(task_gen, args=(INDEX, RUN_TIME,))
             cnt += 1
     p.close()
     p.join()
     print('average task run time is: {:.2f} ms'.format(sum(RUN_TIME) / TOTAL_JOB))
-
